8fb9713bbb00b5dcbc98ee9d6598332a9ae93d0c
whitewolf8383 <kandt.vandf@gmail.com>
2021-09-16 18:21:58 -0600

    added modifiers and created array

--- will.sol
+++ will.sol
@@ -4,5 +4,23 @@ contract Will {
   address owner;
   uint fortune;
   bool deceased;
-  
+
+  constructor() payable public {
+    owner = msg.sender;
+    fortune = msg.value;
+    deceased = false;
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  modifier onlyIfDeceased {
+    require(deaseased == true);
+    _;
+  }
+
+  address payable[] familyWallets
+
 }
